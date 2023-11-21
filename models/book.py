363283dc8fcacd819fadc12135d2db9c03a01d88
moulineE <elmahdi.mouline@live.fr>
2023-11-21 22:25:50 +0100

    db dev 1

--- models/book.py
+++ models/book.py
@@ -5,14 +5,14 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Date, Text, Integer, Table
 from sqlalchemy.orm import relationship
 
-
 book_pages = Table('book_pages', Base.metadata,
                    Column('book_id', String(60), ForeignKey('books.id'),
                           nullable=False, primary_key=True),
                    Column('page_no', Integer,
                           nullable=False, primary_key=True),
-                   Column('content', Text, nullable=False))
+                   Column('content', Text, nullable=False)
                    )
+
 
 class Book(BaseModel, Base):
     """Book class"""
@@ -22,7 +22,6 @@ class Book(BaseModel, Base):
     book_summary = Column(Text, nullable=False)
     author_id = Column(String(60), ForeignKey('authors.id'), nullable=False)
 
-    ef
-    __init__(self, *args, **kwargs):
-    """initializes book"""
-    super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """initializes book"""
+        super().__init__(*args, **kwargs)
