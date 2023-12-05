a53edd6b032836a4ad65cffe2a59fbc2c1e7ffc5
moulineE <elmahdi.mouline@live.fr>
2023-12-05 13:20:17 +0100

    add styling for index.html and book.html with the corresponding css files + js modif for link to book styling

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     for (const book of books) {
       let author = (await fetch('/mylibrary/api/v1/authors/' + book.author_id));
       author = await author.json();
-      html += `<li><h2><a href="/mylibrary/book?id=${book.id}"> ${book.book_title}</a></h2><h3>${author.first_name + ' ' + author.last_name}</h3><h4>published the ${book.published_date}</h4><p>${book.book_summary}</p></li>`;
+      html += `<li><a style="text-decoration:none" href="/mylibrary/book?id=${book.id}"><h2> ${book.book_title}</h2><h3>${author.first_name + ' ' + author.last_name}</h3><h4>published the ${book.published_date}</h4><p>${book.book_summary}</p></a></li>`;
     }
     $('UL.booksResult').html(html);
   });
